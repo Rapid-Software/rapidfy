b3624f8184a4c821e2e353ea30fab94fb6d3e8bb
James <thejamesash1@gmail.com>
2021-07-24 16:22:12 -0400

    add credentials module

--- lib/rapidfy/credentials.ex
+++ lib/rapidfy/credentials.ex
@@ -2,4 +2,26 @@ defmodule Rapidfy.Credentials do
   @moduledoc """
   Provides functions and structures for spotify tokens.
   """
+
+  alias Rapidfy.Credentials
+
+  @doc """
+    Credential struct.
+  """
+  defstruct [:access_token, :refresh_token]
+
+  @doc """
+    Create a Credential struct from provided tokens.
+  """
+  def create_creds(a, r) do
+    %Credentials{access_token: a, refresh_token: r}
+  end
+
+  @doc """
+    Create a Credential struct from spotify auth response.
+  """
+  def res_to_creds(res) do
+    Credentials.create_creds(res["access_token"], res["refresh_token"])
+  end
+
 end
