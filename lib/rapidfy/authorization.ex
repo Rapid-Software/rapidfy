36f6ad0ed4007920db81c56c5b5382b0bcafa839
James <thejamesash1@gmail.com>
2021-07-23 23:31:31 -0400

    start authorization

--- lib/rapidfy/authorization.ex
+++ lib/rapidfy/authorization.ex
@@ -6,4 +6,24 @@ defmodule Rapidfy.Authorization do
   auth url and callback url.
   """
 
+  def get_url() do
+    scopes = get_scopes()
+
+    if scopes != "" do
+      "https://accounts.spotify.com/authorize?client_id=#{Rapidfy.client_id()}&response_type=code&redirect_uri=#{
+        Rapidfy.get_callback_url()
+      }&scope=#{scopes}"
+    else
+      "https://accounts.spotify.com/authorize?client_id=#{Rapidfy.client_id()}&response_type=code&redirect_uri=#{
+        Rapidfy.get_callback_url()
+      }"
+    end
+  end
+
+  def get_scopes() do
+    Application.get_env(:rapidfy, :scopes)
+    |> Enum.join(" ")
+    |> URI.encode()
+  end
+
 end
