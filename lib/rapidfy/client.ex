906b0c82b21576e8419c70486af444e61ea0ab06
James <thejamesash1@gmail.com>
2021-07-26 00:07:43 -0400

    add client methods

--- lib/rapidfy/client.ex
+++ lib/rapidfy/client.ex
@@ -3,4 +3,22 @@ defmodule Rapidfy.Client do
   Provides API Methods to the library.
   """
 
+  def get() do
+
+  end
+
+  def put() do
+
+  end
+
+  @doc false
+  def get_h() do
+
+  end
+
+  @doc false
+  def put_h() do
+
+  end
+
 end
