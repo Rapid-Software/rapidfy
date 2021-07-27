aded22c44fa5ff84cbedd741b2b444918c268524
James <thejamesash1@gmail.com>
2021-07-26 20:16:04 -0400

    add profile functions

--- lib/rapidfy/profile.ex
+++ lib/rapidfy/profile.ex
@@ -5,6 +5,13 @@ defmodule Rapidfy.Profile do
   Provides functions and structures for spotify profiles.
   """
 
+  @url "https://api.spotify.com/v1/"
+
+  alias Rapidfy.{
+    Client,
+    Profile
+  }
+
   defstruct ~w[
     country
     display_name
@@ -19,4 +26,22 @@ defmodule Rapidfy.Profile do
     type
     uri
   ]a
+
+  def get_profile(creds) do
+    url = get_profile_url()
+    Client.get(creds, url) |> Rapidfy.handle_response(%Profile{})
+  end
+
+  def get_profile(creds, id) do
+    url = get_user_profile_url(id)
+    Client.get(creds, url) |> Rapidfy.handle_response(%Profile{})
+  end
+
+  def get_profile_url() do
+    "#{@url}/me"
+  end
+
+  def get_user_profile_url(id) do
+    "#{@url}/users/#{id}"
+  end
 end
